01be43e644592a6eb0eb4081462f42ea9ca2b21b
HARUMI JANG <harumi.jang@acquia.com>
2022-09-15 14:07:51 -0400

    Run prettier

--- starters/basic-starter/lib/get-menus.ts
+++ starters/basic-starter/lib/get-menus.ts
@@ -1,16 +1,15 @@
 import { DrupalMenuLinkContent } from 'next-drupal';
 import { drupal } from '../lib/drupal';
-import {JsonApiWithLocaleOptions} from "next-drupal/dist/types";
-import {GetStaticPropsContext} from "next";
+import { JsonApiWithLocaleOptions } from 'next-drupal/dist/types';
+import { GetStaticPropsContext } from 'next';
 
-export async function getMenus(context?: GetStaticPropsContext,): Promise<{
+export async function getMenus(context?: GetStaticPropsContext): Promise<{
   main: DrupalMenuLinkContent[];
   footer: DrupalMenuLinkContent[];
 }> {
-
   let options: JsonApiWithLocaleOptions = {};
   if (context) {
-    options = {defaultLocale: context.defaultLocale, locale: context.locale};
+    options = { defaultLocale: context.defaultLocale, locale: context.locale };
   }
 
   const { tree: mainMenu } = await drupal.getMenu('main', options);
